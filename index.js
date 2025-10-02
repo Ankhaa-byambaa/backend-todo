d4ff9f40fafd8e37463f5f6d2d89b72d775bcf5e
25LP5026 25LP5026 <25LP5026@G301-CC0203.local>
2025-10-02 12:30:22 +0800

    q

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-
-app.get("/", (req, res) => {
+const arr = app.get("/", (req, res) => {
   res.send("Hi~ !");
 });
 
